69d1651f198dced3a0061be1da2d75590012b43f
Polo dev <polodev10@gmail.com>
2016-09-10 09:45:29 +0530

    changed content

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,3 @@
-//var gulp = require('gulp') ;
-//var less = require('gulp-less');
-//var path = require('path');
-//
-//gulp.task('less', function () {
-//	return gulp.src('assets/style.less')
-//		.pipe(less({
-//			paths : [path.join(__dirname, 'less', 'includes')]
-//		}))
-//		.pipe(gulp.dest('./assets/'));
-//});
-
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
